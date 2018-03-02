aa41c6c99dd26db4d94e12f69384cb758006560a
alvaro cortes <frijoles4me@gmail.com>
2018-03-02 10:51:24 -0800

    updates part14

--- client-app/src/components/App.js
+++ client-app/src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
-
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
 import Home from "./Home";
+import { connect } from "react-redux";
+import Login from "./Login";
+
+const mapStateToProps = state => ({
+  appName: state.appName
+});
 
 class App extends Component {
   render() {
-    return <Home />;
+    return (
+      <div>
+        <Header appName={this.props.appName} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </div>
+    );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, () => ({}))(App);
