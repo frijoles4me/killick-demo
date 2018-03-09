b023d387b28c7585e6c89819bbe8d69dd67b00b0
alvaro cortes <frijoles4me@gmail.com>
2018-03-09 13:06:45 -0800

    part 39 updates

--- client-app/src/components/Article/index.js
+++ client-app/src/components/Article/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "react-router";
+//import { Link } from "react-router";
 import { connect } from "react-redux";
 import marked from "marked";
 
 import services from "../../services";
 
+import ArticleMeta from "./ArticleMeta";
 import CommentContainer from "./CommentContainer";
 
 const mapStateToProps = state => ({
@@ -46,12 +47,15 @@ marked is a library that compiles markdown into HTML - in order to get react to 
       return null;
     }
     const markup = { __html: marked(article.body) };
-    // const canModify = this.props.currentUser.username === article.author.username;
+    const canModify =
+      this.props.currentUser &&
+      this.props.currentUser.username === article.author.username;
     return (
       <div className="article-page">
         <div className="banner">
           <div className="container">
             <h1>{article.title}</h1>
+            <ArticleMeta article={this.props.article} canModify={canModify} />
           </div>
         </div>
 
